56777de0bc08a1070240685b308e54c7bd38fcb9
ZhouSusan <szhou089@gmail.com>
2022-09-26 22:41:23 -0700

    fixed side-nav materialize.css moblie bug

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,12 +1,13 @@
-$(()=>{
-    $('.parallax').parallax();
-    $('.sidenav').sidenav();
-    $('.disabled').on('click', (e) => e.preventDefault())
-  })
-  
-  
-  $(window).on('load', () => { // makes sure the whole site is loaded
+ $(window).on('load', () => { // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(350).css({'overflow':'visible'});
   })
+
+$(()=>{
+  $('.parallax').parallax();
+  $('.sidenav').sidenav({
+    "onOpenStart": () => $(".sidenav-overlay").css("z-index", 0),
+  });
+  $('.disabled').on('click', (e) => e.preventDefault())
+}) 
